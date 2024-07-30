8fc0b56f6f8e7dc365588baeecaaad6ad191b14f
bekzodjonsharobiddinov <bekzodjonsharobiddinov@gmail.com>
2024-07-30 11:07:37 +0500

    project prepared

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { TelegramBotModule } from './telegram_bot/telegram_bot.module';
 import { TelegramBotService } from './telegram_bot/telegram_bot.service';
 import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, 
+    }),
+    TelegrafModule.forRootAsync({
+      useFactory: () => ({
+        token: process.env.TELEGRAM_BOT_TOKEN,
+        launchOptions: {
+          webhook: {
+            polling: true,
+            domain: process.env.TELEGRAM_BOT_TOKEN,
+            path: '/telegram_circle',
+          }
+        }
+      }),
     }),
     TelegramBotModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const bot = app.get(getBotToken());
+  app.use(bot.webhookCallback("/"));
   await app.listen(process.env.APP_PORT);
   console.log("Project is running... PORT: " + process.env.APP_PORT)
 }
